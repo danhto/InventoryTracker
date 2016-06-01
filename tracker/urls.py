cc49847ab823ff631161655b4fc00e6cff9bb56e
Dan To <danhto07@gmail.com>
2016-06-01 17:55:31 -0400

    Added new views
    -Can now add new products to database
    -Can delete existing products from db (needs more testing)

--- tracker/urls.py
+++ tracker/urls.py
@@ -9,4 +9,8 @@ urlpatterns = [
                url(r'^(?P<product_name>[\w\s]+)/product_inventory/$', views.product_inventory, name='product_inventory'),
                # ex: /tracker/add_product
                url(r'^add_product/$', views.add_product, name='add_product'),
+               # ex: /tracker/new_product
+               url(r'^new_product/$', views.new_product, name='new_product'),
+               # ex: /tracker/delete_product
+               url(r'^delete_product/$', views.delete_product, name='delete_product'),
                ]
--- tracker/views.py
+++ tracker/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 from tracker.models import Product, Inventory
+from django.core.urlresolvers import reverse
 
 # List all inventory entries in system
 def index(request):
@@ -25,4 +26,22 @@ def product_inventory(request, product_name):
     return render(request, 'tracker/product_inventory.html', {'inventory_list': inventory_list, 'product_name': p_name})
 
 def add_product(request):
-    return render(request, 'tracker/add_product.html')
+    return render(request, 'tracker/add_product.html', {'product_list': Product.objects.all()})
+    
+def new_product(request):
+    product_name = request.POST['product_name']
+    sm_lot_number = request.POST['sm_lot_number']
+    if product_name.strip() != '' and sm_lot_number.strip() != '':
+        product = Product(product_name=product_name, sm_lot_number=sm_lot_number)
+        print(product)
+        product.save()
+    else:
+        return render(request, 'tracker/add_product.html', {'product_list': Product.objects.all(), 'error_message': "Product name and lot number cannot be empty.",})
+    return HttpResponseRedirect(reverse('tracker:add_product', args=()))
+
+def delete_product(request):
+    product_name = request.POST['product_name']
+    for product in Product.objects.all():
+        if product.product_name == product_name:
+            product.delete()
+    return HttpResponseRedirect(reverse('tracker:add_product', args=()))
