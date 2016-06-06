744fd53296ab132aa32f2fc96a904dc577b0501c
Dan To <danhto07@gmail.com>
2016-06-06 17:49:07 -0400

    Updated models
    -models for Product and Inventory now hold more values
    -delete_product view is now functioning

--- tracker/models.py
+++ tracker/models.py
@@ -3,18 +3,34 @@ from __future__ import unicode_literals
 from django.db import models
 
 class Product(models.Model):
+    DEXTROSE = 'DEX'
+    GUM = 'GUM'
+    CATEGORIES = (
+                  (DEXTROSE, 'Dextrose'),
+                  (GUM, 'Gum'),)
     product_name = models.CharField(max_length=200)
     sm_lot_number = models.CharField(max_length=200)
+    weight = models.DecimalField(max_digits=6, decimal_places=2)
+    pieces = models.IntegerField()
+    category = models.CharField(max_length=3,choices=CATEGORIES,default=DEXTROSE)
     def __str__(self):
         return self.product_name + " - " + self.sm_lot_number
 
 class Inventory(models.Model):
+    NO_LABEL = 0
+    HAS_LABEL = 1
+    SOME_LABELS = 2
+    LABEL_INFO = ((NO_LABEL, 'No Label'),
+                  (HAS_LABEL, 'Has labels'),
+                  (SOME_LABELS, 'Partially labelled'),)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     add_date = models.DateTimeField('date added')
     lot_number = models.CharField(max_length=200)
     quantity = models.IntegerField(default=0)
     location = models.CharField(max_length=100)
-    pieces = models.IntegerField(default = 0)
+    label = models.CharField(max_length=1,choices=LABEL_INFO, default=NO_LABEL)
+    standard = models.BooleanField(default='true')
+    dessicate = models.BooleanField(default='false')
     notes = models.CharField(max_length=200)
     def __str__(self):
         return self.product.product_name + " - " + str(self.lot_number) + " -quantity: " + str(self.quantity) + " -location: " + self.location
--- tracker/views.py
+++ tracker/views.py
@@ -14,7 +14,7 @@ def product_inventory(request, product_name):
     try:
         inventory_all = Inventory.objects.all()
         inventory_list = []
-        p_name = ""
+        p_name = ''
         # Search for matching product_name
         for itm in inventory_all:
             if itm.product.product_name.lower() == product_name:
@@ -25,23 +25,30 @@ def product_inventory(request, product_name):
         raise Http404("No inventory in system")
     return render(request, 'tracker/product_inventory.html', {'inventory_list': inventory_list, 'product_name': p_name})
 
+# Add product calls the add_product.html view
 def add_product(request):
     return render(request, 'tracker/add_product.html', {'product_list': Product.objects.all()})
-    
+
+# Creates and store product based on values entered into the new_product form
 def new_product(request):
     product_name = request.POST['product_name']
     sm_lot_number = request.POST['sm_lot_number']
-    if product_name.strip() != '' and sm_lot_number.strip() != '':
-        product = Product(product_name=product_name, sm_lot_number=sm_lot_number)
+    weight = request.POST['weight']
+    pieces = request.POST['pieces']
+    category = request.POST['category']
+    # Proceed with product creation only if both fields are not empty
+    if product_name.strip() != '' and sm_lot_number.strip() != '' and weight.strip() != '' and pieces.strip() != '':
+        product = Product(product_name=product_name, sm_lot_number=sm_lot_number, weight=weight, pieces=pieces, category=category)
         print(product)
         product.save()
     else:
         return render(request, 'tracker/add_product.html', {'product_list': Product.objects.all(), 'error_message': "Product name and lot number cannot be empty.",})
     return HttpResponseRedirect(reverse('tracker:add_product', args=()))
 
+# Removes product from database
 def delete_product(request):
     product_name = request.POST['product_name']
     for product in Product.objects.all():
-        if product.product_name == product_name:
+        if str(product.product_name) == product_name:
             product.delete()
     return HttpResponseRedirect(reverse('tracker:add_product', args=()))
